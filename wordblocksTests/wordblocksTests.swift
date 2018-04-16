cdf47629803c550ae749b00e7c64a81a39e59c9b
Arpit Agarwal <arpit.agarwal@zomato.com>
2018-04-16 17:09:02 +0530

    Added comments for test cases

--- wordblocksTests/wordblocksTests.swift
+++ wordblocksTests/wordblocksTests.swift
@@ -10,23 +10,38 @@ import XCTest
 @testable import wordblocks
 
 class wordblocksTests: XCTestCase {
+    //TODO:INITIALIZE GLOBAL STATE MANAGER
     
     override func setUp() {
         super.setUp()
+        //set initial turn
+        //set initial state of data manager
+        //load high score
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
+        //TODO:view did appear in 10 seconds
+        
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        //TODO:RESET GLOBAL STATE
+        //TODO:RESET GLOBAL STATE
+        
         super.tearDown()
     }
     
     func testExample() {
+        //
+        
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
+        //TODO:view did appear in 10 seconds
+        
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
